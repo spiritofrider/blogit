38597b4372b79e83d8011834591feb0ac4ccb167
Udit Vikhe <udit.vikhe@gmail.com>
2021-05-22 16:17:21 +0530

    successful deployment changes after

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCdVXvjimz2wCvsN89xjCGRQuUtrJXoW3o",
+    authDomain: "blogit-81857.firebaseapp.com",
+    projectId: "blogit-81857",
+    storageBucket: "blogit-81857.appspot.com",
+    messagingSenderId: "764574602759",
+    appId: "1:764574602759:web:e3c3b15ba932ca2c5bbca2",
+    measurementId: "G-40W8F1ZTSE"
+  }
 };
